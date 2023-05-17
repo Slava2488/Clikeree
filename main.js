d3e3cfbc200d238dc893e63a92274a633913e4ad
Slava2488 <slavakrapivka874@gmail.com>
2023-05-17 09:47:19 +0300

    jjkkk

---
+++ main.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+
+let text = document.querySelector("#Text");
+let text2 = document.querySelector("#Text2");
+let text3 = document.querySelector("#Text3");
+let button = document.querySelector("#Button");
+let button2 = document.querySelector("#Button2");
+let button3 = document.querySelector("#Button3");
+let card = document.querySelector(".card");
+
+
+card.addEventListener('click', () => text())
+}
+
+text = () =>{
+    UpdateUI
+} 
